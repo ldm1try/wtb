99a89fd22981e0be9a86bf6673e8d99e5f8d69a5
Dmitry Lesnyak <dimm4eg@gmail.com>
2022-12-11 16:18:18 +1000

    feat: edit guest.blade.php template & index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,8 @@
 <x-guest-layout>
-    <x-slot name="title">Want To Bye</x-slot>
+  <x-slot name="title">Want To Bye</x-slot>
 
-    <h1>mainpage</h1>
+
+  <h1>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium deserunt earum tempora repellat obcaecati cumque. Quidem dignissimos saepe officia illo?
+  </h1>
 </x-guest-layout>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -2,23 +2,37 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ $title ?? config('app.name') }}</title>
+  <title>{{ $title ?? config('app.name') }}</title>
 
-    <!-- Fonts -->
-    <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+  <!-- Fonts -->
+  <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
-    <!-- Scripts -->
-    @vite(['resources/scss/app.scss', 'resources/css/app.css', 'resources/js/app.js'])
+  <!-- Scripts -->
+  @vite(['resources/scss/app.scss', 'resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body>
-    <div class="font-sans text-gray-900 antialiased">
-        {{ $slot }}
-    </div>
+<body class="font-sans text-gray-900 antialiased">
+  <div class="flex flex-col h-screen">
+    <header class="px-3 border-b border-gray-300">
+      <div class="flex justify-between py-3">
+        <div class="font-bold">{{ config('app.name') }}</div>
+        <div class="">2</div>
+        <div class="">3</div>
+      </div>
+    </header>
+    <main class="flex-auto px-3 py-5">
+      {{ $slot }}
+    </main>
+    <footer class="px-3 border-t border-gray-300 text-center">
+      <div class="py-3">
+        {{ config('app.name') }} - @php echo(date("Y")) @endphp
+      </div>
+    </footer>
+  </div>
 </body>
 
 </html>
